a0a4482ee1e94e1761cbdfccf161eec54f432ea8
apavlov <apavlov@rs>
2012-04-04 10:22:44 +0600

    Repair linux compilation

--- libed2k/include/is_crypto.hpp
+++ libed2k/include/is_crypto.hpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <cryptopp/config.h>
+#ifdef WIN32
 #include <windows.h>
+#endif
 
 // include config.h for byte definition
 // special functions for encrypt and decrypt passwords
@@ -8,7 +10,7 @@
 // used for backward compatibility
 namespace is_crypto
 {
-
+// it is simple ported code - define windows types
 #ifndef WIN32
     typedef unsigned int DWORD;
     typedef unsigned int UINT;
