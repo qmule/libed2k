80b1de6c05e764457dea3e08bbee6a6f5af7c0c4
Ivan Sharov <isharov@che-d-113.(none)>
2012-08-20 19:25:48 +0600

    fix includes

--- src/transfer.cpp
+++ src/transfer.cpp
@@ -1,14 +1,14 @@
-#include "libed2k/version.hpp"
-#include "libed2k/session.hpp"
-#include "libed2k/session_impl.hpp"
-#include "libed2k/transfer.hpp"
-#include "libed2k/peer.hpp"
-#include "libed2k/peer_connection.hpp"
-#include "libed2k/server_connection.hpp"
-#include "libed2k/constants.hpp"
-#include "libed2k/util.hpp"
-#include "libed2k/file.hpp"
-#include "libed2k/alert_types.hpp"
+#include <libed2k/version.hpp>
+#include <libed2k/session.hpp>
+#include <libed2k/session_impl.hpp>
+#include <libed2k/transfer.hpp>
+#include <libed2k/peer.hpp>
+#include <libed2k/peer_connection.hpp>
+#include <libed2k/server_connection.hpp>
+#include <libed2k/constants.hpp>
+#include <libed2k/util.hpp>
+#include <libed2k/file.hpp>
+#include <libed2k/alert_types.hpp>
 
 namespace libed2k
 {
