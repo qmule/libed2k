e789c8924171e46b0825dccc88abf4bd8edc377f
a-pavlov <dkfsoft@gmail.com>
2016-05-24 20:44:32 +0500

    Version up

--- include/libed2k/version.hpp
+++ include/libed2k/version.hpp
@@ -1,11 +1,11 @@
 #ifndef __LIBED2K_VERSION__
 #define __LIBED2K_VERSION__
 
-#define LIBED2K_VERSION_MAJOR 0
+#define LIBED2K_VERSION_MAJOR 1
 #define LIBED2K_VERSION_MINOR 1
 #define LIBED2K_VERSION_TINY  0
 
-#define LIBED2K_VERSION "0.1.0.0"
+#define LIBED2K_VERSION "1.1.0"
 #define LIBED2K_REVISION "$Rev: 1 $"
 
 // the format of this version is: MMmmtt
